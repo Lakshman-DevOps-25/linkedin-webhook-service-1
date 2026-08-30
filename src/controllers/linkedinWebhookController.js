908290005402548dc0edd51b851f72544433297c
Lakshman-DevOps-25 <laxmanaraog@gmail.com>
2026-08-30 22:37:17 +0530

    Update linkedinWebhookController.js

--- src/controllers/linkedinWebhookController.js
+++ src/controllers/linkedinWebhookController.js
@@ -802,10 +802,39 @@ const processLinkedInData =
 
 };
 
+
+const testWebhook = async (req, res) => {
+
+  console.log("");
+  console.log("========================================");
+  console.log("TEST WEBHOOK RECEIVED");
+  console.log("========================================");
+
+  console.log("Method:", req.method);
+
+  console.log(
+    "Headers:",
+    JSON.stringify(req.headers, null, 2)
+  );
+
+  console.log(
+    "Body:",
+    JSON.stringify(req.body, null, 2)
+  );
+
+  console.log("========================================");
+
+  return res.status(200).json({
+    success: true,
+    message: "Test webhook received successfully",
+    received: true
+  });
+};
+
 /*
 |--------------------------------------------------------------------------
 | Exports
 |--------------------------------------------------------------------------
 */
 
-module.exports = {validateWebhook, receiveWebhook, testLinkedInData};
+module.exports = {validateWebhook, receiveWebhook, testLinkedInData, testWebhook};
